3dee30916656e8d0ade0f282f11f39e4b0b654cb
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-08-31 17:59:58 +0900

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    root 'users/sessions#new'
+  end
+
+  devise_for :users, :controllers => {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
