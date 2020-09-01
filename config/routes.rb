c43121be39e9d5274aafb1be63bb8a20dd56c342
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-09-01 17:49:35 +0900

    Add user/show edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_scope :user do
     root 'users/sessions#new'
   end
@@ -6,5 +8,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     sessions: 'users/sessions'
   }
+
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
