cf2e32e8b6b9b69c71f8730d23e099ec0ca433d9
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-09-03 15:55:29 +0900

    Add log create

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,19 @@
 class LogsController < ApplicationController
   def new
+    @user = current_user
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+    @log.user_id = current_user.id
+    @log.save
+    redirect_to user_path(current_user)
+  end
+
+
+  private
+  def log_params
+    params.require(:log).permit(:date, :weight, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @logs = Log.where(user_id: current_user)
   end
 
   def edit
