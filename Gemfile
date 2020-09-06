4a6a37e162a8015ec86260830c5b2da25d5de181
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-09-06 19:08:34 +0900

    Add chartkick

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'bootstrap'
 gem 'gon'
+gem 'chartkick'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @logs = Log.where(user_id: current_user)
-    gon.logs = Log.where(user_id: current_user)
   end
 
   def edit
