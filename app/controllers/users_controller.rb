7c0d621aae80e10aaa411d25e65c91f90444c2cb
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-09-09 12:47:28 +0900

    Add graph

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def show
     @logs = Log.where(user_id: current_user)
+    @data = []
+
+    @logs.each do |log|
+      @data.push([log.date, log.weight])
+    end
+
+
   end
 
   def edit
