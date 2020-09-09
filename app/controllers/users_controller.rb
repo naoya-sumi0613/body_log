50914c9c310c301d349a271c429d220761b18a76
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-09-09 14:29:57 +0900

    Fix graph

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
   def show
-    @logs = Log.where(user_id: current_user)
+    @logs = Log.where(user_id: current_user).order(date: "ASC")
     @data = []
+    @today = Date.today
+    @min = 1000
+    @max = 0
 
     @logs.each do |log|
       @data.push([log.date, log.weight])
+      if @min > log.weight
+        @min = log.weight
+      elsif @max < log.weight
+        @max = log.weight
+      end
     end
 
 
